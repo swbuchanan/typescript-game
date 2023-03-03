0a4f986db2155156c44fb365e54a4c9d4e73f005
steven <stevenman42@gmail.com>
2023-03-03 18:09:31 -0500

    aoeu

--- public/src/greeter.js
+++ public/src/greeter.js
@@ -1,23 +1,14 @@
-class Student {
-  fullName: string;
-  constructor(
-    public firstName: string,
-    public middleInitial: string,
-    public lastName: string
-  ) {
-    this.fullName = firstName + " " + middleInitial + " " + lastName;
-  }
+var Student = /** @class */ (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+    return Student;
+}());
+function greeter(person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
 }
- 
-interface Person {
-  firstName: string;
-  lastName: string;
-}
- 
-function greeter(person: Person) {
-  return "Hello, " + person.firstName + " " + person.lastName;
-}
- 
-let user = new Student("Jane", "M.", "User");
- 
+var user = new Student("Jane", "M.", "User");
 document.body.textContent = greeter(user);
